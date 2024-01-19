9ec6a503f23778dd1dfef7cd0fe19e2d3ce17bba
Ekram Hosen <ekramhosen329@gmail.com>
2024-01-19 22:48:38 +0600

    zod schema

--- src/components/Form/NormalForm.tsx
+++ src/components/Form/NormalForm.tsx
@@ -1,13 +1,23 @@
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const signUpForm = z.object({
+  name: z.string().min(1, { message: "Name is required!" }),
+  email: z.string().email().min(1, { message: "Email is required!" }),
+  password: z.string().min(8, { message: "Password is to short!" }),
+});
+
+type NormalForm = z.infer<typeof signUpForm>;
 
 const NormalForm = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<NormalForm>({ resolver: zodResolver(signUpForm) });
 
-  const onSubmit = (data: unknown) => {
+  const onSubmit = (data: FieldValues) => {
     console.log(data);
   };
   return (
@@ -20,7 +30,7 @@ const NormalForm = () => {
           {...register("name", { required: true })}
         />
         {errors.name && (
-          <span className="text-red-600">This field is required!</span>
+          <span className="text-red-600">{errors.name.message}</span>
         )}
         <label htmlFor="name">Email</label>
         <input
@@ -29,7 +39,7 @@ const NormalForm = () => {
           {...register("email", { required: true })}
         />
         {errors.email && (
-          <span className="text-red-600">This field is required!</span>
+          <span className="text-red-600">{errors.email.message}</span>
         )}
         <label htmlFor="name">Pasword</label>
         <input
@@ -37,7 +47,9 @@ const NormalForm = () => {
           id="password"
           {...register("password", { minLength: 8 })}
         />
-        {errors.password && <span className="text-red-600">Too short!</span>}
+        {errors.password && (
+          <span className="text-red-600">{errors.password.message}</span>
+        )}
       </div>
       <button type="submit">Submit</button>
     </form>
