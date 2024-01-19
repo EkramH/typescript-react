b2d63a1f16aa4a49988999556d4c2e0e64db7737
Ekram Hosen <ekramhosen329@gmail.com>
2024-01-19 22:00:04 +0600

    form validation

--- src/components/Form/NormalForm.tsx
+++ src/components/Form/NormalForm.tsx
@@ -1,7 +1,11 @@
 import { useForm } from "react-hook-form";
 
 const NormalForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data: unknown) => {
     console.log(data);
@@ -10,7 +14,30 @@ const NormalForm = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" {...register("name")} />
+        <input
+          type="text"
+          id="name"
+          {...register("name", { required: true })}
+        />
+        {errors.name && (
+          <span className="text-red-600">This field is required!</span>
+        )}
+        <label htmlFor="name">Email</label>
+        <input
+          type="text"
+          id="email"
+          {...register("email", { required: true })}
+        />
+        {errors.email && (
+          <span className="text-red-600">This field is required!</span>
+        )}
+        <label htmlFor="name">Pasword</label>
+        <input
+          type="text"
+          id="password"
+          {...register("password", { minLength: 8 })}
+        />
+        {errors.password && <span className="text-red-600">Too short!</span>}
       </div>
       <button type="submit">Submit</button>
     </form>
