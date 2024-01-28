b59d8e43d7f7cb047919912e65e7627b46d95bcd
Ekram Hosen <ekramhosen329@gmail.com>
2024-01-28 10:01:44 +0600

    navbar fixed and animated

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,15 @@
 import { NavLink } from "react-router-dom";
 import { Button } from "../ui/button";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <header className="h-16">
+    <motion.header
+      className="h-16 fixed w-full bg-white z-[999]"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1, duration: 1 }}
+    >
       <nav className="w-full max-w-[1230px] mx-auto h-full px-5 flex justify-between items-center ">
         <span className="text-3xl">iRepair</span>
         <ul className="space-x-4">
@@ -12,7 +18,7 @@ const Navbar = () => {
           <Button>Login</Button>
         </ul>
       </nav>
-    </header>
+    </motion.header>
   );
 };
 
--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -25,10 +25,13 @@ const introChildren = {
 };
 
 const laptop = {
-  initial: { y: 0 },
+  initial: { y: 0, rotate: 0, scale: 5 },
   animate: {
     y: 20,
+    rotate: -30,
+    scale: 1,
     transition: {
+      duration: 1,
       y: {
         duration: 2,
         repeat: Infinity,
@@ -40,41 +43,45 @@ const laptop = {
 };
 const HeroSection = () => {
   return (
-    <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center">
-      <motion.div variants={intro} initial="hidden" animate="visible">
-        <motion.h1
-          className="text-5xl lg:text-8xl font-bold text-nowrap"
-          variants={introChildren}
-        >
-          <span className="text-gray">Don't worry.</span>
-          <br />
-          <span>We'll fix it.</span>
-        </motion.h1>
-        <motion.p
-          className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg"
-          variants={introChildren}
-        >
-          Welcome to{" "}
-          <span className="text-primary-foreground font-semibold">iRepair</span>
-          , your one-stop place for all kinds of{" "}
-          <span className="text-primary-foreground font-semibold">
-            Macbook repairs
-          </span>
-          and diagnostics.
-        </motion.p>
-        <motion.div variants={introChildren}>
-          <Button>Book a service</Button>
+    <div className="overflow-hidden">
+      <Container className="h-screen pt-16 grid grid-cols-1 lg:grid-cols-2 place-content-center">
+        <motion.div variants={intro} initial="hidden" animate="visible">
+          <motion.h1
+            className="text-5xl lg:text-8xl font-bold text-nowrap"
+            variants={introChildren}
+          >
+            <span className="text-gray">Don't worry.</span>
+            <br />
+            <span>We'll fix it.</span>
+          </motion.h1>
+          <motion.p
+            className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg"
+            variants={introChildren}
+          >
+            Welcome to{" "}
+            <span className="text-primary-foreground font-semibold">
+              iRepair
+            </span>
+            , your one-stop place for all kinds of{" "}
+            <span className="text-primary-foreground font-semibold">
+              Macbook repairs
+            </span>
+            and diagnostics.
+          </motion.p>
+          <motion.div variants={introChildren}>
+            <Button>Book a service</Button>
+          </motion.div>
         </motion.div>
-      </motion.div>
-      <motion.div
-        variants={laptop}
-        initial="initial"
-        animate="animate"
-        className="mt-10 w-3/4 lg:w-full mx-auto"
-      >
-        <img className="-rotate-[35deg]" src={macbook} alt="" />
-      </motion.div>
-    </Container>
+        <motion.div
+          variants={laptop}
+          initial="initial"
+          animate="animate"
+          className="mt-10 w-3/4 lg:w-full mx-auto"
+        >
+          <img className="h-[95%] object-contain" src={macbook} alt="" />
+        </motion.div>
+      </Container>
+    </div>
   );
 };
 
